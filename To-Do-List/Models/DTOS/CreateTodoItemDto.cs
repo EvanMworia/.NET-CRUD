392634023a99303982baa3b40a78c34062122bb1
EvanMworia <evannji99@gmail.com>
2025-06-05 23:36:10 +0300

    Chore: Added DTOs in business layer

---
+++ To-Do-List/Models/DTOS/CreateTodoItemDto.cs
@@ -0,0 +1,11 @@
+﻿using static To_Do_List.Models.TodoItem;
+
+namespace To_Do_List.Models.DTOS
+{
+    public class CreateTodoItemDto
+    {
+        public required string Title { get; set; }
+        public required DateTime DueDate { get; set; }
+        public ItemStatus Status { get; set; }
+    }
+}
