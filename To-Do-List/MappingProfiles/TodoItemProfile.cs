be8c27eda75cc5713d36a4d1f6ddf566009266fb
EvanMworia <evannji99@gmail.com>
2025-06-05 23:41:54 +0300

    Feat: Setup mapping profiles using Automapper and registered it to the DI Container

---
+++ To-Do-List/MappingProfiles/TodoItemProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using To_Do_List.Models;
+using To_Do_List.Models.DTOS;
+
+namespace To_Do_List.MappingProfiles
+{
+    public class TodoItemProfile:Profile
+    {
+        public TodoItemProfile()
+        {
+            CreateMap<CreateTodoItemDto, TodoItem>().ReverseMap();
+        }
+    }
+}
--- To-Do-List/Program.cs
+++ To-Do-List/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using To_Do_List.Data;
+using To_Do_List.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+//2.--------REGISTERING OUR SERVICES, <Interface, Implementation>
+builder.Services.AddScoped<ITodo, TodoService>();
+
+//3.-------REGISTERING AUTOMAPPER
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
