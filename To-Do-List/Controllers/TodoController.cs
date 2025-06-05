4775f2f2a061eafd0451c466f07d8b2a0f4caa1b
EvanMworia <evannji99@gmail.com>
2025-06-05 23:41:01 +0300

    Chore: Implemented HttpPost CreateTodoItem function

---
+++ To-Do-List/Controllers/TodoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using To_Do_List.Models;
+using To_Do_List.Models.DTOS;
+using To_Do_List.Services;
+
+namespace To_Do_List.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodo _todoService;
+        private readonly ResponseDto _responseDto;
+        public TodoController(ITodo todo)
+        {
+            _todoService = todo;
+            _responseDto = new ResponseDto();
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseDto>> CreateNewItem([FromBody] CreateTodoItemDto itemDto)
+        {
+
+            try
+            {
+                var res = await _todoService.CreateTodoItem(itemDto);
+                if (!res.IsSuccess)
+                {
+                    return BadRequest(res);
+                }
+                return Ok(res);
+
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+
+                return BadRequest(_responseDto.Message = ex.Message);
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null) { 
+                    return BadRequest(_responseDto.Message = $"{ex.InnerException.Message}");
+
+                }
+                return BadRequest(_responseDto.Message = $"{ex.Message}");
+            }
+        }
+    }
+}
